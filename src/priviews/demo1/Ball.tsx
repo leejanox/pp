b5898174f2ea3054a4516bfdb5bde91f99af3583
goeun.Kim <rhdms6748@gmail.com>
2025-05-15 12:47:58 +0900

    preview add

--- src/priviews/demo1/Ball.tsx
+++ src/priviews/demo1/Ball.tsx
@@ -1,7 +1,7 @@
 import { forwardRef, useMemo}  from "react"
 import * as THREE from 'three'
-import vert from './vert.glsl?raw'
-import frag from './frag.glsl?raw'
+//import vert from './vert.glsl?raw'
+//import frag from './frag.glsl?raw'
 
 interface BallProps {
     position?: [number, number, number]
@@ -21,8 +21,58 @@ const Ball = forwardRef<THREE.Points,BallProps>(({...props},ref) => {
             uMouse : { value: new THREE.Vector3(0,0,0) },
             uVolume: { value: 0. },
         },
-        vertexShader:vert,
-        fragmentShader:frag,
+        vertexShader:`
+        varying vec2 vUv;
+varying vec3 vPosition;
+
+uniform float uTime;
+uniform vec3 uMouse;
+uniform vec3 uColor;
+
+void main() {
+
+    /*
+    float speed = .4;
+
+    vec3 base = uColor;
+    vec3 custom = base + vec3(
+        .2 * sin(uTime * .1 + .5) + speed,
+        .2 * sin(uTime * .7 + .5) ,
+        .2 * sin(uTime * .5 + .5) + speed/2.
+    ); 
+    //gl_FragColor = vec4(custom, a);
+    */
+    vec3 base = vec3(1.,1.,1.);
+    float a = 1.;
+
+    gl_FragColor = vec4(base, a*.4);
+}`,
+        fragmentShader:`
+        varying vec2 vUv;
+varying vec3 vPosition;
+
+uniform float uTime;
+uniform vec3 uMouse;
+uniform vec3 uColor;
+
+void main() {
+
+    /*
+    float speed = .4;
+
+    vec3 base = uColor;
+    vec3 custom = base + vec3(
+        .2 * sin(uTime * .1 + .5) + speed,
+        .2 * sin(uTime * .7 + .5) ,
+        .2 * sin(uTime * .5 + .5) + speed/2.
+    ); 
+    //gl_FragColor = vec4(custom, a);
+    */
+    vec3 base = vec3(1.,1.,1.);
+    float a = 1.;
+
+    gl_FragColor = vec4(base, a*.4);
+}`,
         side:THREE.DoubleSide,
         wireframe:true,
         transparent:true,
